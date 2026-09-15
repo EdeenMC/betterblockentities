7ecbd75b8f893f7b9912c76fa3eab142a9264bfd
ceeden <emilcedendahl@gmail.com>
2026-09-15 19:59:48 +0200

    version bumps

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -12,15 +12,15 @@ object BuildConfig {
 
     //fabric loader and api version
     const val FABRIC_LOADER_VERSION: String = "0.19.3"
-    const val FABRIC_API_VERSION: String = "0.160.3+26.3"
+    const val FABRIC_API_VERSION: String = "0.160.5+26.3"
 
     //minecraft version
-    const val MINECRAFT_VERSION: String = "26.3-rc-1"
+    const val MINECRAFT_VERSION: String = "26.3"
 
     //sodium version (needs to vary between snapshot builds and releases)
     //because of the different artifact naming schemes
-    const val SODIUM_VERSION: String = "0.9.2-beta.2+mc26.3r1"
+    const val SODIUM_VERSION: String = "0.9.2+mc26.3"
 
     //BBE mod version (remember to bump!!!)
-    const val MOD_VERSION: String = "1.3.8-beta.1"
+    const val MOD_VERSION: String = "1.3.9"
 }
